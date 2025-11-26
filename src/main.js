8753d4be9b0e4dc2a217fd0ba01b2ca234679d76
Takeru Kakuta <kakuta19960915@gmail.com>
2025-11-26 18:53:35 +0900

    feat: JSX相当の描画をバニラJSで実装

--- src/main.js
+++ src/main.js
@@ -1,24 +1,40 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// React ====================
+// function MyComponent() {
+//   return <h1>こんにちは</h1>;
+// }
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// const container = document.getElementById("root");
+// ReactDOM.render(<MyComponent />, container); // ここでレンダリングされる
+//  =========================
 
-setupCounter(document.querySelector('#counter'))
+// 上記のJSXが裏側で実際に処理される内容 (JSがほしい形に変換している) ==========
+// const element = React.createElement("h1", { title: "foo" }, "こんにちは");
+// const container = document.getElementById("root");
+// ReactDOM.render(element, container);
+// ===================================================================
+
+// createElementをJSに置き換え =========
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "こんにちは",
+  },
+};
+
+const container = document.getElementById("root");
+
+// renderをJSに置き換え
+const node = document.createElement(element.type); // h1要素を取得
+node["title"] = element.props.title; // h1の属性にfooを入れる
+
+const text = document.createTextNode(""); // 空のテキストノードを作成
+text["nodeValue"] = element.props.children; // childrenプロパティを代入
+
+node.appendChild(text); // 子要素として追加
+
+container.appendChild(node);
+
+// メモ
+// ・裏側でviteが自動でcreateElementを作成
+// ・nodeを作ってcontainerに追加することでdomに追加できる
