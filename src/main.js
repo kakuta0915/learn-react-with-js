095b438d4573a7c99faec998bcc0545b032ad2cb
Takeru Kakuta <kakuta19960915@gmail.com>
2025-11-27 18:26:10 +0900

    remove: 不要なコメントアウトを削除

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 // JSXを、仮想DOM要素オブジェクトに変換するための関数
-// 📝 React.createElement とほぼ同じ役割を持つ。
-// 📝 children を配列形式で統一することで、後の処理（ループ・再帰）が書きやすくなる。
 function createElement(type, props, ...children) {
   return {
     type,
@@ -8,8 +6,6 @@ function createElement(type, props, ...children) {
       ...props,
 
       // 受け取った child のうち、文字列は TEXT_ELEMENT に変換
-      // 📝「文字列と要素を同じ構造で扱うため」に必要。
-      // 📝 Reactもこの形に変換して扱っている
       children: children.map((child) =>
         typeof child === "object" ? child : createTextElement(child)
       ),
@@ -18,44 +14,26 @@ function createElement(type, props, ...children) {
 }
 
 // テキストノードを仮想DOMオブジェクト形式に変換する関数
-// 📝 本来ブラウザのテキストノードは特別扱いだが、Reactは統一処理のために TEXT_ELEMENT という疑似タグに変換している。
 function createTextElement(text) {
   return {
     type: "TEXT_ELEMENT", // ダミーのタグ名（実際のDOMには存在しない）
     props: {
-      nodeValue: text, // テキスト内容を保持
+      nodeValue: text,
       children: [], //  テキストは子要素を持たないので空配列
     },
   };
 }
 
-// JSXを createElement に置き換えた仮想DOMオブジェクト
 const element = createElement("h1", { title: "foo" }, "こんにちは");
-
-// 表示先の DOM要素（#root）を取得
 const container = document.getElementById("root");
 
-// element.type から実際の DOM要素を作成
-// 📝「仮想DOM → 本物のDOM」への変換作業に相当する。
 const node = document.createElement(element.type);
-
-// element.props の中にある属性 title を h1 に設定
-// node.title = "foo" と同義。
-// 📝 Reactは全 props をループしてまとめて setAttribute している。
-// ⚠️ 本来は setAttribute を使うのが無難な場合もある。
 node["title"] = element.props.title;
 
-// テキストノードを空で作成
-// ここで空にする理由は「後で nodeValue に文字を代入するため」。
-// createTextNode に直接文字を渡しても良いが、学習のため分けている。
 const text = document.createTextNode("");
-
-// TEXT_ELEMENT の text.props.nodeValue を DOM テキストノードへ反映
-// nodeValue に値を代入するとテキストが表示される。
 text["nodeValue"] = element.props.children;
 
 // h1 の中にテキストノードを追加
-// → <h1 title="foo">こんにちは</h1> が形成される。
 node.appendChild(text);
 
 // 最後に h1 を #root の中に配置してブラウザに表示
